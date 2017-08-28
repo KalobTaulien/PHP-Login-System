6f0c0e80f9c1d2bd1e0c7aa6c6591c8e9e638c1c
Kalob Taulien <kalob.taulien@gmail.com>
2017-08-27 18:47:55 -0600

    Added extension ideas to this project

--- dashboard.php
+++ dashboard.php
@@ -31,6 +31,20 @@
   		<h2>Dashboard</h2>
       <p>Hello <?php echo $User->email; ?>, you registered at <?php echo $User->reg_time; ?></p>
       <p><a href="/logout.php">Logout</a></p>
+      <p>Ideas for extending this system:</p>
+      <ul>
+        <li>NEW FEEDS</li>
+        <li>FRIEND FEED</li>
+        <li>CHANGE EMAIL</li>
+        <li>CHANGE PASSWORD</li>
+        <li>RESET PASSWORD</li>
+        <li>INVITE MODULE </li>
+        <li>ADD FIRST NAME</li>
+        <li>ADD LASTNAME</li>
+        <li>ADD EMAIL CONFIRMATION</li>
+        <li>ADD SMS CONFIRMATION (VIA TWILIO)</li>
+      </ul>
+      <p>Pick one of these and build it yourself. It'll be fun! Don't forget to ask questions in our group: <a href="https://www.facebook.com/groups/1088943884540928/">https://www.facebook.com/groups/1088943884540928/</a></p>
   	</div>
 
   	<?php require_once "inc/footer.php"; ?> 
