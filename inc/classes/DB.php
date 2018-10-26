ebac23f77996d61c03f4d28f1e030cb3a502d7e9
AeroPwn <30424201+AeroPwn@users.noreply.github.com>
2018-10-26 14:27:41 -0700

    Update DB.php

--- inc/classes/DB.php
+++ inc/classes/DB.php
@@ -15,7 +15,9 @@ class DB {
 
 			self::$con = new PDO( 'mysql:charset=utf8mb4;host=localhost;port=3306;dbname=login_course', 'root', 'root' );
 			self::$con->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+			self::$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);//uncomment on production sites
 			self::$con->setAttribute( PDO::ATTR_PERSISTENT, false );
+			self::$con->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
 		} catch (PDOException $e) {
 			echo "Could not connect to database."; exit;
